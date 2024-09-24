81fa77e1bdc317ca49ffff8d4385bafb64588741
Martin Matovu <matovius@proton.me>
2024-09-24 13:31:14 +0300

    add app container instead of the BasicCanvas component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,10 @@
   import "@fontsource/bungee";
   import "@fontsource-variable/nunito";
   import "../styles/app.css";
-	import BasicCanvas from "$lib/components/BasicCanvas.svelte";
-
 </script>
 
 <svelte:head>
   <title>Whiteboarder</title>
 </svelte:head>
 
-<BasicCanvas />
+<div class="app"></div>
